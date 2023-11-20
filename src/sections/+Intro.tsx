057cf3513da4bc46c55b8a98644f593c5b16784c
Aardhyn Lavender <83677410+AardhynLavender@users.noreply.github.com>
2023-11-21 11:13:53 +1300

    chore: update

--- src/sections/+Intro.tsx
+++ src/sections/+Intro.tsx
@@ -5,17 +5,16 @@ export function Intro() {
     <Root>
       <H1>
         This is <G warm>aardhyn.dev</G>.
-        <br />A personal website and playground on the web
+        <br />A personal website and playground on the web.
       </H1>
       <P>
-        I'm a <G warm>Front-end Software Developer</G> in small team developing
-        a website builder, and final year{" "}
-        <G cool>Bachelor of Information Technology</G> student.
+        I'm a <G warm>Software Developer</G> in small team developing
+        a website builder and CMS platform.
       </P>
       <P>
-        I have a love for software engineering with <G cool>React.js</G> and
+        I have a love for software engineering with <G cool>React</G> and
         <G cool> TypeScript</G>, and low-level application and game engine
-        development with <G cool>C++20</G> and <G cool>Web Assembly</G>.
+        development with <G cool>C++20</G> and dable in <G cool>Web Assembly</G>.
       </P>
     </Root>
   );
