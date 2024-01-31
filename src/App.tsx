443830d4dcfb4e222eeacd23584edf4062eb85ce
Aardhynlavneder <aardhyn@gmail.com>
2024-01-31 20:11:25 +1300

    fix: footer not sticky

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,14 @@ export default function App() {
     <Main>
       <Header />
       <Intro />
+      <Greedy />
       <Footer />
     </Main>
   );
 }
 const Main = styled("main", {
   minHeight: "100vh",
+  display: "flex",
+  flexDirection: "column",
 });
+const Greedy = styled("div", { flex: 1 });
