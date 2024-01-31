a6e8bff9b3c9896a0babe987a37b6ff879e829b2
Aardhynlavneder <aardhyn@gmail.com>
2024-01-31 20:28:13 +1300

    feat: gradient refinement

--- src/sections/+Intro.tsx
+++ src/sections/+Intro.tsx
@@ -8,18 +8,33 @@ export function Intro() {
         <br />A personal website and playground on the web.
       </H1>
       <P>
-        I'm a <G warm>Software Developer</G> in small team developing
-        a website builder and CMS platform.
+        I'm a{" "}
+        <G warm strong>
+          Software Developer
+        </G>{" "}
+        in small team developing a website builder and CMS platform.
       </P>
       <P>
-        I have a love for software engineering with <G cool>React</G> and
-        <G cool> TypeScript</G>, and low-level application and game engine
-        development with <G cool>C++20</G> and dable in <G cool>Web Assembly</G>.
+        I'm work primarily in{" "}
+        <G cool strong>
+          {" "}
+          React{" "}
+        </G>{" "}
+        and{" "}
+        <G cool strong>
+          {" "}
+          TypeScript{" "}
+        </G>
+        day to day, and dabble in game development and systems programming in my
+        spare time.
       </P>
     </Root>
   );
 }
 const Root = styled("section", {
+  display: "flex",
+  flexDirection: "column",
+  gap: 24,
   margin: "0 auto",
   padding: "128px 16px",
   maxWidth: 800,
@@ -28,13 +43,17 @@ const Root = styled("section", {
 
 const P = styled("p", {
   color: "#fff",
+  fontWeight: 400,
 });
+
+const WARM = "linear-gradient(90deg, #ff5d5d 0%, #5d6dff 100%)";
+const COOL = "linear-gradient(90deg, #19cdd6 0%, #82d56c 100%)";
 
 const G = styled("span", {
   variants: {
     warm: {
       true: {
-        background: "-webkit-linear-gradient(180deg, #da8b25, #c150a2)",
+        background: WARM,
         backgroundClip: "text",
         WebkitBackgroundClip: "text",
         WebkitTextFillColor: "transparent",
@@ -42,10 +61,15 @@ const G = styled("span", {
     },
     cool: {
       true: {
-        background: "-webkit-linear-gradient(180deg, #19cdd6, #82d56c)",
+        background: COOL,
         backgroundClip: "text",
         WebkitBackgroundClip: "text",
         WebkitTextFillColor: "transparent",
+      },
+    },
+    strong: {
+      true: {
+        fontWeight: 600,
       },
     },
   },
